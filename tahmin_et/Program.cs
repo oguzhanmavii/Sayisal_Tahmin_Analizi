815ecc62d2a2cfde92c39ff893b799f9fe136eac
OĞUZHAN MAVİ <mavioguz09@gmail.com>
2021-10-27 10:24:34 +0300

    Sayisal_Tahmin_Analizi

---
+++ tahmin_et/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tahmin_et
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Random randomSayi = new Random(); // rastgele sayiyi tanimlamak için kullanilan fonksiyon :)
+            int rastegeleSayi = randomSayi.Next(1, 1000);// buradaki parametre rastgele gelecek olan sayinin 1 ile 1000 arasinda olmasını sağlar 
+            Console.WriteLine("Sayiyi Tahmin Ediniz :");
+            int tahmin = Convert.ToInt32(Console.ReadLine());
+            int tahminSayaci = 1;
+            while(rastegeleSayi != tahmin)
+            {
+                if (rastegeleSayi > tahmin)
+                {
+                    Console.WriteLine("tahmin ettiginiz sayidan büyüktür !");
+                }
+                else
+                {
+                    Console.WriteLine("tahmin ettiginiz sayidan küçüktür");
+                }
+                Console.WriteLine("Sayiyi tekrardan tahmin ediniz");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+                tahminSayaci++;
+            }
+            Console.WriteLine("Tebrikler Sayiyi Doğru Tahmin ettiniz");
+            Console.WriteLine("Sayiyi Tahmin edene kadar " + tahminSayaci + " kere denediniz ");
+
+            Console.ReadLine();
+        }
+    }
+}
